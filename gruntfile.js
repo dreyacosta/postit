3fef5531fcddf4ae258ed24a6ae3ad3cddfd49ee
David Rey <david.rey.acosta@gmail.com>
2013-03-30 20:47:54 +0100

    More tasks added

--- gruntfile.js
+++ gruntfile.js
@@ -4,6 +4,10 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		jshint: {
+			all: ['Gruntfile.js', 'routes/*.js', 'routes/admin/*.js', 'public/javascripts/*.js']
+		},
+
 		uglify: {
 			compress: {
 				files: {
@@ -19,14 +23,21 @@ module.exports = function(grunt) {
 				// options: { cwd: 'path/to/sources' },
 				files: { 'deploy/': ['public/font/*', 'public/images/**', 'public/stylesheets/**', 'routes/**', 'views/**', 'app.js', 'package.json'] }
 			}
+		},
+
+		watch: {
+			files: ['routes/**', 'public/javascripts/*.js'],
+			tasks: ['jshint']
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	// Default task(s).
-	grunt.registerTask('default', ['uglify', 'copy']);
+	grunt.registerTask('default', ['jshint', 'uglify', 'copy']);
 
 };
