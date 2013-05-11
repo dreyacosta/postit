03be2813b574f8b6b2a3aa758f38179c195ddba3
David Rey <david.rey.acosta@gmail.com>
2013-05-11 13:55:24 +0200

    Indentation convert to spaces

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,53 +1,53 @@
 module.exports = function(app, config, post, user, pass, functions) {
-	
-	/*******************
-	 * Admin home page
-	 *******************/
+    
+    /*******************
+     * Admin home page
+     *******************/
 
-	app.get(config.url.admin.index, function (req, res) {
-		res.render('admin/index', {
-			title: 'Administration | ' + config.blogName,
-			description: config.blogName + ' admin page',
-			session: req.session,
-			config: config
-		});
-		req.session.msg = '';
-	});
+    app.get(config.url.admin.index, function (req, res) {
+        res.render('admin/index', {
+            title: 'Administration | ' + config.blogName,
+            description: config.blogName + ' admin page',
+            session: req.session,
+            config: config
+        });
+        req.session.msg = '';
+    });
 
-	// Login form
-	
-	app.post(config.url.admin.index, function(req, res) {
-		var username = req.param('user');
-		var password = req.param('pass');
+    // Login form
+    
+    app.post(config.url.admin.index, function(req, res) {
+        var username = req.param('user');
+        var password = req.param('pass');
 
-		functions.getUserByUsername(username, function (user) {
-			if (user) {
-				pass.hash(password, user.salt, function(err, hash){
-					if (user.pass == hash) {
-						req.session.regenerate(function(){
-							req.session.username = user.username;
-							res.redirect('back');
-						});
-					}
-				});
-			} else {
-				req.session.msg = 'Incorrect username or password';
-				res.render('admin/index', {
-					title: 'Administration | ' + config.blogName,
-					description: config.blogName + ' admin page',
-					session: req.session,
-					config: config
-				});
-				req.session.msg = '';
-			}
-		});
-	});
+        functions.getUserByUsername(username, function (user) {
+            if (user) {
+                pass.hash(password, user.salt, function(err, hash){
+                    if (user.pass == hash) {
+                        req.session.regenerate(function(){
+                            req.session.username = user.username;
+                            res.redirect('back');
+                        });
+                    }
+                });
+            } else {
+                req.session.msg = 'Incorrect username or password';
+                res.render('admin/index', {
+                    title: 'Administration | ' + config.blogName,
+                    description: config.blogName + ' admin page',
+                    session: req.session,
+                    config: config
+                });
+                req.session.msg = '';
+            }
+        });
+    });
 
-	// Logout
+    // Logout
 
-	app.get(config.url.admin.logout, function(req, res) {
-		req.session.destroy();
-		res.redirect('/');
-	});
+    app.get(config.url.admin.logout, function(req, res) {
+        req.session.destroy();
+        res.redirect('/');
+    });
 
 };
--- routes/admin/users.js
+++ routes/admin/users.js
@@ -1,64 +1,64 @@
 module.exports = function(app, config, post, user, pass, functions) {
 
-	/**************************
-	 * Users administration
-	 **************************/
+    /**************************
+     * Users administration
+     **************************/
 
-	app.get(config.url.admin.users, function (req, res) {
-		functions.getUsers(function (users) {
-			res.render('admin/users', {
-				title: 'Users administration | ' + config.blogName,
-				description: 'Users page',
-				session: req.session,
-				users: users,
-				config: config
-			});
-		});
-	});
+    app.get(config.url.admin.users, function (req, res) {
+        functions.getUsers(function (users) {
+            res.render('admin/users', {
+                title: 'Users administration | ' + config.blogName,
+                description: 'Users page',
+                session: req.session,
+                users: users,
+                config: config
+            });
+        });
+    });
 
-	// New user
+    // New user
 
-	app.get(config.url.create.user, function (req, res) {
-		functions.getUsers(function (users) {
-			res.render('admin/new_user', {
-				title: 'New user | ' + config.blogName,
-				description: 'Users page',
-				session: req.session,
-				users: users,
-				config: config
-			});
-		});
-	});
+    app.get(config.url.create.user, function (req, res) {
+        functions.getUsers(function (users) {
+            res.render('admin/new_user', {
+                title: 'New user | ' + config.blogName,
+                description: 'Users page',
+                session: req.session,
+                users: users,
+                config: config
+            });
+        });
+    });
 
-	// Edit user
+    // Edit user
 
-	app.get(config.url.edit.user + '/:id', function (req, res) {
-		var id = req.param('id');
+    app.get(config.url.edit.user + '/:id', function (req, res) {
+        var id = req.param('id');
 
-		functions.getUserById(id, function (user) {
-			res.render('admin/edit_user', {
-				title: 'User edition | ' + config.blogName,
-				description: 'Users page',
-				session: req.session,
-				user: user,
-				config: config
-			});
-		});
-	});
+        functions.getUserById(id, function (user) {
+            res.render('admin/edit_user', {
+                title: 'User edition | ' + config.blogName,
+                description: 'Users page',
+                session: req.session,
+                user: user,
+                config: config
+            });
+        });
+    });
 
-	// Remove user
+    // Remove user
 
-	app.get(config.url.remove.user + '/:id', function (req, res) {
-		var id = req.param('id');
+    app.get(config.url.remove.user + '/:id', function (req, res) {
+        var id = req.param('id');
 
-		if (req.session.username) {
-			functions.getUserById(id, function (user) {
-				user.remove();
-				res.redirect(config.url.admin.users);
-			});
-		} else {
-			res.redirect(config.url.admin.index);
-		}
-	});
+        if (req.session.username) {
+            functions.getUserById(id, function (user) {
+                user.remove();
+                res.redirect(config.url.admin.users);
+            });
+        } else {
+            res.redirect(config.url.admin.index);
+        }
+    });
 
 };
