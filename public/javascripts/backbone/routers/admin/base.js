38ed2e33dce55f537a42ca7f7a494d3b6b19f474
David Rey <david.rey.acosta@gmail.com>
2013-06-22 20:55:06 +0200

    Admin router code improvements

--- public/javascripts/backbone/routers/admin/base.js
+++ public/javascripts/backbone/routers/admin/base.js
@@ -9,176 +9,257 @@ Postit.Routers.AdminRouter = Backbone.Router.extend({
     },
 
     initialize: function(app) {
-        this.articles = app.articles;
-        this.users = app.users;
+        console.log('Initialize admin router', app);
+
+        // this.articles = app.articles;
+        // this.users = app.users;
 
         if (!this.menuView) {
-            this.menuView = new Postit.Views.Menu();
-            this.menuView.render().$el.appendTo('#sidebar');
+            app.$el = $('#menu');
+
+            this.menuView = new Postit.Views.Menu(app);
+            // this.menuView.render().$el.appendTo('#sidebar');
         }
 
-        Postit.articles.on('add', function(article) {
-            app.model = article;
-            app.state = "articles";
+        // Postit.articles.on('add', function(article) {
+        //     console.log('Router add collection');
+        //     app.model = article;
+        //     app.state = "articles";
 
-            var articleView = new Postit.Views.Model(app);
-            articleView.render().$el.prependTo('#articles');
-        });
+        //     var articleView = new Postit.Views.Model(app);
+        //     articleView.render().$el.prependTo('#articles');
+
+        //     app.state = '';
+        // });
     },
  
     reset: function(){
-        console.log('Router homeAdmin');
+        console.log('Router reset', app);
 
-        if (this.addArticleView) {
-            this.addArticleView.destroy();
-            delete this.addArticleView;
+        if (this.articlesView) {
+            console.log('articlesView exist');
+
+            this.articlesView.destroy();
+            delete this.articlesView;
         }
 
-        if (this.articlesListView) {
-            this.articlesListView.destroy();
-            delete this.articlesListView;
+        if (this.usersView) {
+            console.log('usersView exist');
+
+            this.usersView.destroy();
+            delete this.usersView;
         }
-        
+
         if (this.newArticleView) {
+            console.log('newArticleView exist');
+
             this.newArticleView.destroy();
             delete this.newArticleView;
         }
 
-        if (this.articleEditionView) {
-            this.articleEditionView.destroy();
-            delete this.articleEditionView;
-        }
+        // if (this.newArticleView) {
+        //     this.newArticleView.destroy();
+        //     delete this.newArticleView;
+        // }
 
-        if (this.addUserView) {
-            this.addUserView.destroy();
-            delete this.addUserView;
-        }
+        // if (this.articleEditionView) {
+        //     this.articleEditionView.destroy();
+        //     delete this.articleEditionView;
+        // }
 
-        if (this.userListView) {
-            this.userListView.destroy();
-            delete this.userListView;
-        }
+        // if (this.addUserView) {
+        //     this.addUserView.destroy();
+        //     delete this.addUserView;
+        // }
 
-        if (this.userEditionView) {
-            this.userEditionView.destroy();
-            delete this.userEditionView;
-        }
+        // if (this.userEditionView) {
+        //     this.userEditionView.destroy();
+        //     delete this.userEditionView;
+        // }
+
+        // $('[data-role="addButton"]').html('');
     },
     
     showArticles: function() {
-        console.log('Router showArticles');
+        console.log('Router showArticles', app);
 
+        
+        
         if (app.state != "articles") {
             this.reset();
+
             app.state = "articles";
+            app.$el = $('#articles');
+
+            this.articlesView = new Postit.Views.Collection(app);
         }
 
-        if (!this.addArticleView) {
-            this.addArticleView = new Postit.Views.Add(app);
-            this.addArticleView.render().$el.appendTo('#sidebar');
-        }
+        $('html').attr('class', 'isList');
 
-        if (!this.articlesListView) {
-            this.articlesListView = new Postit.Views.List();
-            this.articlesListView.render().$el.appendTo('#sidebar');
+        // if (app.state != "articles") {
+        //     this.reset();
+        //     app.state = "articles";
 
-            this.articles.each(function(article){
-                app.model = article;
+        //     this.articles.each(function(article){
+        //         app.model = article;
 
-                var articleView = new Postit.Views.Model(app);
-                articleView.render().$el.appendTo('#articles');
-            });
-        }
+        //         var articleView = new Postit.Views.Model(app);
+        //         articleView.render().$el.appendTo('#articles');
+        //     });
+
+        //     app.$el = $('[data-role="addButton"]');
+
+        //     this.addArticleView = new Postit.Views.Add(app);
+        //     this.addArticleView.render().$el;
+        // }
+
+        // var byCategory = this.articles.where({category: "Demo"});
+
+        // for (var article in byCategory) {
+        //     console.log(byCategory[article].toJSON());
+        // }
     },
 
     newArticle: function () {
-        console.log('Router newArticle');
+        console.log('Router newArticle', app);
 
-        if (app.state != "articles") {
-            this.reset();
-            this.showArticles();
-            app.state = "articles";
-        }
+        
 
-        app.state = "articles";
+        this.showArticles();
 
         this.newArticleView = new Postit.Views.New();
         $('#container').html(this.newArticleView.render().el);
+
+        $('html').attr('class', 'isContent');
+
+        // if (app.state != "articles") {
+        //     this.reset();
+        //     this.showArticles();
+        //     app.state = "articles";
+        // }
+
+        // app.state = "articles";
+
+        // this.newArticleView = new Postit.Views.New();
+        // $('#container').html(this.newArticleView.render().el);
     },
 
     article: function(id) {
-        console.log('Router article ' + id);
+        console.log('Router article', app, id);
 
-        if (app.state != "articles") {
-            this.reset();
-            this.showArticles();
-            app.state = "articles";
-        }
+        
 
-        var model = this.articles.find(function(article) {
+        this.showArticles();
+
+        var article = app.articles.find(function(article) {
             return article.get('_id') === id;
         });
 
-        if (!model) {
+        if (!article) {
             console.log('Article not found');
             return;
         }
 
-        app.model = model;
-
+        app.model = article;
         this.articleEditionView = new Postit.Views.Edition(app);
         $('#container').html(this.articleEditionView.render().el);
+
+        $('html').attr('class', 'isContent');
+
+        // if (app.state != "articles") {
+        //     this.reset();
+        //     this.showArticles();
+        //     app.state = "articles";
+        // }
+
+        // var model = this.articles.find(function(article) {
+        //     return article.get('_id') === id;
+        // });
+
+        // if (!model) {
+        //     console.log('Article not found');
+        //     return;
+        // }
+
+        // app.model = model;
+
+        // this.articleEditionView = new Postit.Views.Edition(app);
+        // $('#container').html(this.articleEditionView.render().el);
     },
 
     showUsers: function() {
-        console.log('Router showUsers');
+        console.log('Router showUsers', app);
 
         if (app.state != "users") {
             this.reset();
+
             app.state = "users";
+            app.$el = $('#articles');
+
+            this.usersView = new Postit.Views.Collection(app);
         }
+        
 
-        if (!this.userListView) {
-            this.userListView = new Postit.Views.List(app);
-            this.userListView.render().$el.appendTo('#sidebar');
+        // if (app.state != "users") {
+        //     this.reset();
+        //     app.state = "users";
+        // }
 
-            this.users.each(function(user){ 
-                app.model = user;
+        // this.users.each(function(user){ 
+        //     app.model = user;
 
-                var userView = new Postit.Views.Model(app);
-                userView.render().$el.appendTo('#articles');
-            });
-        }
+        //     var userView = new Postit.Views.Model(app);
+        //     userView.render().$el.appendTo('#articles');
+        // });
 
-        Postit.users.on('add', function(user) {
-            app.model = user;
-            app.state = "articles";
+        // Postit.users.on('add', function(user) {
+        //     app.model = user;
+        //     app.state = "articles";
 
-            var userView = new Postit.Views.Model(app);
-            userView.render().$el.prependTo('#articles');
-        });
+        //     var userView = new Postit.Views.Model(app);
+        //     userView.render().$el.prependTo('#articles');
+        // });
+        
     },
 
     user: function(id) {
-        console.log('Router user ' + id);
+        console.log('Router user', id);
 
-        if (app.state != "users") {
-            this.reset();
-            app.state = "users";
-        }
+        
 
-        var model = this.users.find(function(user) {
+        var user = app.users.find(function(user) {
             return user.get('_id') === id;
         });
 
-        if (!model) {
+        if (!user) {
             console.log('User not found');
             return;
         }
 
-        app.model = model;
+        app.model = user;
 
         this.userEditionView = new Postit.Views.Edition(app);
         $('#container').html(this.userEditionView.render().el);
+
+        $('html').attr('class', 'isContent');
+
+        // if (app.state != "users") {
+        //     this.reset();
+        //     app.state = "users";
+        // }
+
+        // var model = this.users.find(function(user) {
+        //     return user.get('_id') === id;
+        // });
+
+        // if (!model) {
+        //     console.log('User not found');
+        //     return;
+        // }
+
+        // app.model = model;
+
+        // this.userEditionView = new Postit.Views.Edition(app);
+        // $('#container').html(this.userEditionView.render().el);
     }
 });
