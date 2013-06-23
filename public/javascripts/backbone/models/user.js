8c37597fda6792f917e9a85bbaaef2299b00b511
David Rey <david.rey.acosta@gmail.com>
2013-06-23 21:03:15 +0200

    Added fetch URL to users collection

--- public/javascripts/backbone/models/user.js
+++ public/javascripts/backbone/models/user.js
@@ -5,6 +5,15 @@ Postit.Models.User = Backbone.Model.extend({
 });
 
 Postit.Collections.Users = Backbone.Collection.extend({
+    url: "/users",
+
     model: Postit.Models.Article,
-    name: "users"
+
+    name: "users",
+
+    initialize: function() {
+        if (this.size() === 0) {
+            this.fetch();
+        }
+    }
 });
