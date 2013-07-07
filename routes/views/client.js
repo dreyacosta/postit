61e42f07fffe44169bf86da39ab984947ba92d62
David Rey <david.rey.acosta@gmail.com>
2013-07-07 21:54:52 +0200

    More info to client Jade template and added variables for GET request

--- routes/views/client.js
+++ routes/views/client.js
@@ -1,9 +1,46 @@
-module.exports = function(app) {
-    app.get('/', function(req, res) {
+module.exports = function(app, config, query) {
+    
+    if (config.public.url.blog === "") {
+        homeURL = "/";
+    } else {
+        homeURL = config.public.url.blog;
+    }
+
+    app.get(homeURL, function(req, res) {
         console.log(req.user);
+
         res.render('client/index', {
-            title: 'Postit',
-            user: req.user
+            user: req.user,
+            env: config.env,
+            data: {
+                title: config.public.blogName,
+                content: config.public.blogDescription
+            }
         });
     });
-}
+
+    app.get(config.public.url.blog + '/:id', function(req, res) {
+        console.log(req.user);
+        
+        query.getPostBySlug(req.params.id, function(article) {
+            res.render('client/index', {
+                user: req.user,
+                env: config.env,
+                data: article
+            });
+        });
+    });
+
+    app.get(config.public.url.blog + '/category/:id', function(req, res) {
+        console.log(req.user);
+
+        res.render('client/index', {
+            user: req.user,
+            env: config.env,
+            data: {
+                title: config.public.blogName,
+                description: config.public.blogDescription
+            }
+        });
+    });
+};
