760f231537e2f22e9a61dcfa4aff9f93698823e0
David Rey <david.rey.acosta@gmail.com>
2013-07-07 21:58:05 +0200

    More data to admin JADE templates and variables for GET request

--- routes/views/admin.js
+++ routes/views/admin.js
@@ -1,25 +1,51 @@
-module.exports = function(app) {
-    app.get('/admin', function(req, res) {
+module.exports = function(app, config, passport) {
+    app.get(config.public.url.admin, function(req, res) {
         console.log(req.user);
+
         res.render('admin/index', {
             title: 'Postit admin',
+            env: config.env,
             session: req.user
         });
     });
 
-    app.get('/admin/articles', function(req, res) {
+    app.get(config.public.url.admin + config.public.api.articles, function(req, res) {
         console.log(req.user);
+
         res.render('admin/index', {
             title: 'Postit admin',
+            env: config.env,
             session: req.user
         });
     });
 
-    app.get('/admin/articles/:id', function(req, res) {
+    app.get(config.public.url.admin + config.public.api.articles + '/:id', function(req, res) {
         console.log(req.user);
+
         res.render('admin/index', {
             title: 'Postit admin',
+            env: config.env,
             session: req.user
         });
     });
-}
+
+    app.get(config.public.url.admin + config.public.api.users, function(req, res) {
+        console.log(req.user);
+        
+        res.render('admin/index', {
+            title: 'Postit admin',
+            env: config.env,
+            session: req.user
+        });
+    });
+
+    app.get(config.public.url.admin + config.public.api.users + '/:id', function(req, res) {
+        console.log(req.user);
+        
+        res.render('admin/index', {
+            title: 'Postit admin',
+            env: config.env,
+            session: req.user
+        });
+    });
+};
