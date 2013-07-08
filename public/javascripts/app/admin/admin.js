9313dc2b0a083388266c01ada6a520e4af6628c7
David Rey <david.rey.acosta@gmail.com>
2013-07-08 21:44:21 +0200

    admin.js APP load templates, config, collections, routers with promises and new socket event for remove users

--- public/javascripts/app/admin/admin.js
+++ public/javascripts/app/admin/admin.js
@@ -1,5 +1,3 @@
-var appConfig = new $.Deferred();
-
 MyApp = new Backbone.Marionette.Application();
 
 MyApp.on("initialize:after", function(){
@@ -8,13 +6,15 @@ MyApp.on("initialize:after", function(){
     app.adminRouter = new Postit.Routers.AdminRouter();
 
     Backbone.history.start({
-        root : "admin",
+        root : app.config.url.admin,
         pushState : true,
         silent : false
     });
 });
 
 var getTemplates = function(templates, callback) {
+    var dTemplates = Q.defer();
+
     var deferreds = [];
 
     $.each(templates, function(index, view) {
@@ -23,96 +23,50 @@ var getTemplates = function(templates, callback) {
         }, 'html'));
     });
 
-    $.when.apply(null, deferreds).done(callback);
+    $.when.apply(null, deferreds).done(function() {
+        dTemplates.resolve();
+    });
+
+    return dTemplates.promise;
+};
+
+var getConfig = function() {
+    var dConfig = Q.defer();
+
+    var xhr = $.get('/postit/config');
+
+    xhr.done(function(data) {
+        app.config = data;
+        dConfig.resolve();
+    });
+
+    return dConfig.promise;
 };
 
 var getCollections = function() {
+    var dCollections = Q.defer();
+
     app.articles = new Postit.Collections.Articles();
     app.users = new Postit.Collections.Users();
 
     $.when(app.articles.fetch(), app.users.fetch()).done(function() {
-        appConfig.resolve();
+        dCollections.resolve();
     });
+
+    return dCollections.promise;
 };
 
 $(function() {
     var templates = ['article', 'articleEdition', 'articleNew', 'user', 'userEdition', 'sidebar'];
 
-    getTemplates(templates, getCollections);
-
-    appConfig.done(function() {
+    getTemplates(templates).then(function() {
+        return getConfig();
+    }).then(function() {
+        return getCollections();
+    }).then(function() {
         MyApp.start();
         console.log('App started!');
     });
-
-    // app.adminRouter = new Postit.Routers.AdminRouter();
-
-
-    // app.articles = new Postit.Collections.Articles();
-    // app.users = new Postit.Collections.Users();
-
-    // var templates = ['article', 'articleEdition', 'articleNew', 'newButton', 'user', 'userEdition', 'list'];
-
-    // function getTemplates(templates, callback) {
-    //     var deferreds = [];
-
-    //     $.each(templates, function(index, view) {
-    //         deferreds.push($.get('/templates/admin/' + view + '.html', function(data) {
-    //             app.templates[view] = _.template(data);
-    //         }, 'html'));
-    //     });
-
-    //     $.when.apply(null, deferreds).done(callback);
-    // }
-
-    // getTemplates(templates, function() {
-    //     // app.viewArticles = new Postit.Views.Articles({collection: app.articles});
-    //     // $('#list').html(app.viewArticles.render().el);
-
-    //     Postit.adminRouter = new Postit.Routers.AdminRouter();
-
-    //     Backbone.history.start({
-    //         root : "admin",
-    //         pushState : true,
-    //         silent : false
-    //     });
-    // });
-
-    // function loadData(callback) {
-    //     var xhrArticles = $.get('/articles');
-
-    //     xhrArticles.done(function(data){
-    //         console.log('Articles loaded', data);
-
-    //         data.forEach(function(article){
-    //             Postit.articles.add(article);
-    //         });
-
-    //         var xhrUsers = $.get('/users');
-
-    //         xhrUsers.done(function(data){
-    //             console.log('Users loaded', data);
-
-    //             data.forEach(function(user){
-    //                 Postit.users.add(user);
-    //             });
-
-    //             callback();
-    //         });
-
-    //         xhrUsers.fail(function(data) {
-    //             console.log('Users 401');
-    //         });
-    //     });
-    // }
-
-    // loadData(function() {
-    //     Backbone.history.start({
-    //         root : "admin",
-    //         pushState : true,
-    //         silent : false
-    //     });
-    // });
 
     // Sockets events
 
@@ -133,6 +87,11 @@ $(function() {
     socket.on('users::create', function(data) {
         console.log('Users::create ' + data);
         app.users.add(data);
+    });
+
+    socket.on('users::remove', function(data) {
+        console.log('Socket on remove user', data);
+        app.users.fetch();
     });
 
     socket.on('articles::update', function(data) {
