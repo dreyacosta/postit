5cfc21f06f23355f8746bcceae64c299c59d0d0f
David Rey <david.rey.acosta@gmail.com>
2013-07-09 22:54:38 +0200

    html5editor improvements

--- public/javascripts/vendors/html5editor.js
+++ public/javascripts/vendors/html5editor.js
@@ -1,69 +1,94 @@
-var html5editor = (function() {
-    this.editor = document.querySelector("[data-html5editor-role='editor']");
-    this.preview = document.querySelector("[data-html5editor-role='preview']");
-    this.styles = Array.prototype.slice.call(document.querySelectorAll("[data-html5editor-tagName]"));
+var html5editor = function(config) {
 
-    this.styles.forEach(function(style) {
-        style.addEventListener('click', function() {
-            var tagName = this.getAttribute("data-html5editor-tagName");
-            var className = this.getAttribute("data-html5editor-className");
+    var editor = document.querySelector("[" + config.editor + "]");
+    var preview = document.querySelector("[" + config.preview + "]");
+    var tagName = Array.prototype.slice.call(document.querySelectorAll("[" + config.tagName + "]"));
 
-            if (className) {
-                addStyle(tagName, className);
-            } else {
-                addStyle(tagName);
-            }
+    if (!editor) {
+        console.log('You need to define and editor');
+        return;
+    }
+
+    if (tagName) {
+
+        tagName.forEach(function(style) {
+
+            style.addEventListener('click', function() {
+
+                var tagName = this.getAttribute(config.tagName);
+                var className = this.getAttribute(config.className);
+
+                if (className) {
+                    addStyle(tagName, className);
+                } else {
+                    addStyle(tagName);
+                }
+
+            });
+
         });
-    });
 
-    this.editor.addEventListener('keyup', function() {
-        html5editor.linebreaksToParagraphs(html5editor.editor.value);
-    });
-
+    }
+    
     var addStyle = function(tagName) {
+
         var className = arguments[1];
 
-        var editorVal = html5editor.editor.value;
+        var editorVal = editor.value;
 
-        var selected_txt = editorVal.substring(html5editor.editor.selectionStart, html5editor.editor.selectionEnd);
+        var selected_txt = editorVal.substring(editor.selectionStart, editor.selectionEnd);
             
-        var before_txt = editorVal.substring(0, html5editor.editor.selectionStart);
-        var after_txt = editorVal.substring(html5editor.editor.selectionEnd, editorVal.length);
+        var before_txt = editorVal.substring(0, editor.selectionStart);
+        var after_txt = editorVal.substring(editor.selectionEnd, editorVal.length);
             
         if (className) {
-            html5editor.editor.value = before_txt + '<' + tagName + ' class="' + className + '">' + selected_txt + '</' + tagName + '>' + after_txt;
+            editor.value = before_txt + '<' + tagName + ' class="' + className + '">' + selected_txt + '</' + tagName + '>' + after_txt;
         } else {
-            html5editor.editor.value = before_txt + '<' + tagName + '>' + selected_txt + '</' + tagName + '>' + after_txt;
+            editor.value = before_txt + '<' + tagName + '>' + selected_txt + '</' + tagName + '>' + after_txt;
         }
 
-        html5editor.linebreaksToParagraphs(html5editor.editor.value);
+        var formattedContent = html5editor.lineBreak(editor.value);
+
+        if (preview) {
+            preview.innerHTML = formattedContent;
+        }
+
     }
 
-    return {
-        editor: this.editor,
-        preview: this.preview,
-        linebreaksToParagraphs: function(editorVal) {
-            var formattedContent = "";
-            var paragraphs = html5editor.editor.value.split('\n');
+    editor.addEventListener('keyup', function() {
+        var formattedContent = html5editor.lineBreak(editor.value);
 
-            for (var i in paragraphs) {
-                if (paragraphs[i]) {
-                    paragraphs[i] = '<p>' + paragraphs[i] + '</p>';
-                    formattedContent = formattedContent + paragraphs[i];
-                }
-            }
+        if (preview) {
+            preview.innerHTML = formattedContent;
+        }
+    });
 
-            html5editor.preview.innerHTML = formattedContent;
-            return formattedContent;
-        },
-        onLoadFormat: function(dbValue) {
-            var previewVal = dbValue;
-            
-            previewVal = previewVal.replace(/<p>/g, '');
-            previewVal = previewVal.replace(/<\/p>/g, '\n\n');
+};
 
-            previewVal = previewVal.trim();
-            return previewVal;
+html5editor.lineBreak = function(text) {
+
+    var formattedContent = "";
+    var paragraphs = text.split('\n');
+
+    for (var i in paragraphs) {
+        if (paragraphs[i]) {
+            paragraphs[i] = '<p>' + paragraphs[i] + '</p>';
+            formattedContent = formattedContent + paragraphs[i];
         }
     }
-})();
+
+    return formattedContent;
+
+}
+
+html5editor.onLoadFormat = function(dbValue) {
+
+    var previewVal = dbValue;
+    
+    previewVal = previewVal.replace(/<p>/g, '');
+    previewVal = previewVal.replace(/<\/p>/g, '\n\n');
+    previewVal = previewVal.trim();
+
+    return previewVal;
+
+}
