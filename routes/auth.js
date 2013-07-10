ea173dee955b3ef45052ebc7769880804a47d7a4
David Rey <david.rey.acosta@gmail.com>
2013-07-10 23:47:12 +0200

    Added variables to get requests and changes to register user

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,29 @@
-module.exports = function(app, db, passport, TwitterStrategy) {
+module.exports = function(app, config, db, passport, TwitterStrategy) {
     passport.use(new TwitterStrategy({
-            consumerKey: 'wV2KPmz9q0TrgpBBQMmw',
-            consumerSecret: 'd8fbireax06EDCuM0VTd9QTKUb3jHwlUUpzkMHLzPQc',
-            callbackURL: "/auth/twitter/callback"
+            consumerKey: config.twitter.consumerKey,
+            consumerSecret: config.twitter.consumerSecret,
+            callbackURL: config.public.url.authTwitterCallback
         },
-      
+
         function(token, tokenSecret, profile, done) {
             console.log(profile);
             db.User.findOne({uid: profile.id}, function(err, user) {
-            
+
                 if(user) {
                     done(null, user);
                 } else {
-                    var user = new db.User();
-                    
-                    user.uid = profile.id;
-                    user.username = profile.username;
-                    user.fullName = profile.displayName;
-                    user.image = profile._json.profile_image_url;
-                    user.provider = 'Twitter';
-                    user.save(function(err) {
+                    var newUser = new db.User();
+
+                    newUser.uid = profile.id;
+                    newUser.username = profile.username;
+                    newUser.fullName = profile.displayName;
+                    newUser.image = profile._json.profile_image_url;
+                    newUser.provider = 'twitter';
+                    newUser.save(function(err) {
                         if(err) { throw err; }
-                        done(null, user);
+                        done(null, newUser);
                     });
-                    io.sockets.emit('users::create', user);
+                    io.sockets.emit('users::create', newUser);
                 }
             });
         }
@@ -39,13 +39,13 @@ module.exports = function(app, db, passport, TwitterStrategy) {
         });
     });
 
-    app.get('/auth/twitter', passport.authenticate('twitter'), function(req, res){
+    app.get(config.public.url.authTwitter, passport.authenticate('twitter'), function(req, res){
         // The request will be redirected to Twitter for authentication, so this
         // function will not be called.
         console.log(req.user.username);
     });
 
-    app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
-        res.redirect('back');
+    app.get(config.public.url.authTwitterCallback, passport.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
+        res.redirect(config.public.url.admin);
     });
-}
+};
