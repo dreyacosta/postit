653807e8de0b711022eb4540c4210413c5b15ef7
David Rey <david.rey.acosta@gmail.com>
2013-07-11 20:14:13 +0200

    Client router set document title and description automatically and added show articles by category

--- public/javascripts/app/client/routers/router.js
+++ public/javascripts/app/client/routers/router.js
@@ -1,7 +1,8 @@
 Postit.Routers.ClientRouter = Backbone.Router.extend({
     routes: {
         "": "home",
-        ":id": "articlePage"
+        ":id": "articlePage",
+        "category/:id": "category"
     },
 
     initialize: function() {
@@ -12,12 +13,18 @@ Postit.Routers.ClientRouter = Backbone.Router.extend({
         console.log('Router home');
 
         app.state = "articles";
+        console.log(app.articles.toJSON());
 
         this.articlesView = new Postit.Views.Articles({collection: app.articles});
         MyApp.articles.show(this.articlesView);
 
         this.sidebarView = new Postit.Views.Sidebar();
         MyApp.sidebar.show(this.sidebarView);
+
+        document.title = app.config.blogName;
+        $('meta[name=description]').attr('content', app.config.blogDescription);
+
+        jQuery("abbr.timeago").timeago();
     },
 
     articlePage: function(id) {
@@ -25,13 +32,45 @@ Postit.Routers.ClientRouter = Backbone.Router.extend({
 
         app.state = "articlePage";
 
-        var article = app.articles.find(function(article2) {
-            return article2.get('slug') === id;
+        var article = app.articles.find(function(article) {
+            return article.get('slug') === id;
         });
+
+        article.set({views: article.get('views') + 1});
+        article.save();
 
         this.articleView = new Postit.Views.Article({model: article});
         MyApp.articles.show(this.articleView);
 
-        article.save();
+        document.title = article.get("title");
+        $('meta[name=description]').attr('content', article.get("description"));
+
+        jQuery("abbr.timeago").timeago();
+    },
+
+    category: function(id) {
+        console.log('Router category', app.articles);
+
+        app.state = "articles";
+
+        var byCategory = app.articles.where({category: id});
+
+        app.articlesByCategory = new Postit.Collections.Articles();
+
+        for (var article in byCategory) {
+            console.log(byCategory[article].toJSON());
+
+            app.model = byCategory[article];
+
+            app.articlesByCategory.add(app.model);
+        }
+
+        this.articlesView = new Postit.Views.Articles({collection: app.articlesByCategory});
+        MyApp.articles.show(this.articlesView);
+
+        document.title = app.config.blogName;
+        $('meta[name=description]').attr('content', app.config.blogDescription);
+
+        jQuery("abbr.timeago").timeago();
     }
 });
